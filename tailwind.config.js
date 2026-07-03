f3a40a21ea059488b34caf60d9f929b1ef73121b
katherineimbachi044-netizen <katherineimbachi044@gmail.com>
2026-07-03 07:47:43 -0500

    subiendo trabajo

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#39A900',
+        brandPill: '#4CB018',
+        brandDark: '#2E8600',
+        panel: '#1e1e1e',
+      }
+    }
+  },
+  plugins: [],
+}
